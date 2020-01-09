28a38aae7767077b2ad64fe66ce578f9464a4263
LRY1994 <949892801@qq.com>
2020-01-09 18:45:49 +0800

    fix

--- server/clean.js
+++ server/clean.js
@@ -2,8 +2,11 @@ const fs = require('fs');
 const CONFIG = require('./config');
 const Utils = require('./utils')
 
-Utils.deleteFolder(CONFIG.CSS_PATH_DEV);
+
 Utils.deleteFolder('./public');
 if( fs.existsSync(CONFIG.CACHE_PATH) ) {
     fs.unlinkSync(CONFIG.CACHE_PATH);
+}
+if( fs.existsSync(CONFIG.CSS_PATH_DEV) ) {
+    fs.unlinkSync(CONFIG.CSS_PATH_DEV);
 }
--- server/config.js
+++ server/config.js
@@ -10,4 +10,4 @@ exports.CACHE_PATH = './Cache.json'
 exports.CATEGORIES_PATH='./public/categories';
 exports.CSS_PATH='./public/static/css'
 
-exports.CSS_PATH_DEV = './static/css'
+exports.CSS_PATH_DEV = './static/css/all.css'
